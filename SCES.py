691fd73167e77f78e634106aa9b49c9393ca6b05
G.Dias <gustavo.v.dias@edu.senai.br>
2026-06-18 13:54:21 -0300

    Adicionei um código para excluir os produtos

--- SCES.py
+++ SCES.py
@@ -9,7 +9,7 @@ def Alterar():
     
     global produtos
     linhaprocurada =-1
-    alter=input("Qual nome do produto que deseja alterar a quantidade? ")
+    alter=input("Qual nome do produto que deseja alterar a quantidade? ").capitalize()
     for i in range(len(produtos)):##Varre linha a linha da matriz
         if(produtos[i][1]==alter):
             linhaprocurada = i
@@ -18,13 +18,20 @@ def Alterar():
     else:
         print(f"{produtos[linhaprocurada]}")
         novaQuantidade= int(input(f"Qual a nova quantidade do produto?: "))
-        produtos[linhaprocurada][2] = novaQuantidade
+        produtos[linhaprocurada][2] = novaQuantidade ## Muda a quantidade do produto desejado
         print("Nova quantidade atualizada com sucesso!")
         print(f"{produtos[linhaprocurada]}")
     travarmenu()
 
+
 def dados():
-    print("-----------------------------------")
+    for i in range(len(produtos)):##Varre linha a linha da matriz
+        if(produtos[i][0]==ID): ##Verifica se a posição do nome é igual ao nome procurado
+            linhaprocurada = i
+    if (linhaprocurada==-1):
+        print("Produto não eiste.")
+    else:
+        print(f"O Produto procurado é {produtos[linhaprocurada]}")
     print(produtos)
     travarmenu()  
 
@@ -41,22 +48,34 @@ def BuscaProd():
         print(f"O Produto procurado é {produtos[linhaprocurada]}")
     travarmenu()
 
+
 def remProd():##Remover Produtos
     if (len(produtos)==0):
         print("Não ha produtos. Adicione")
     else:
-    
-        produtos.pop(produtos)
-        print("Produto removido.")
-        print(produtos)
-    travarmenu()
-
-
+        ID = int(input("Qual ID do produto que deseja remover? "))
+        for i in range(len(produtos)):##Varre linha a linha da matriz
+            if(produtos[i][0]==ID): ##Verifica se a posição do nome é igual ao nome procurado
+                linhaprocurada = i
+        if (linhaprocurada==-1):
+            print("Produto não eiste.")
+        else:
+            print(f"{produtos[linhaprocurada]}")
+            Apagar=input("Você quer excluir esse produto? Sim/Não ").capitalize()
+            if (Apagar=="Sim"):
+                produtos.pop(linhaprocurada)
+                print("Produto excluido com sucesso 🗑️")
+            else:
+                print("Cancelado !!")
+                
+                    
 def resProd():##Add Produtos
     novoProduto = input("Qual ID e nome do novo , quantidade e localização do produto ? ")
     produtos.append(novoProduto) ## inserimos
     print("Produto inserido com sucesso!")
     travarmenu()
+
+
 
 def travarmenu():
     #Nosso código vai aqui
@@ -67,7 +86,7 @@ def travarmenu():
 
 print("\nBem vindo ao menu interativo do estoque. Por favor selecione uma opção:")
 while True : #Roda para sempre
-    print("\n1- Buscar produtos | 2- Alterar| 3- Produtos| 4- Novo produto | 5-Remover produto | 6-Sair")
+    print("\n1- Buscar produtos | 2- Alterar qauntidades | 3- Status do estoque | 4- Novo produto | 5-Remover produto | 6-Sair")
     opcao=input("Escolha: ")
     if (opcao=="1"):
         BuscaProd()
